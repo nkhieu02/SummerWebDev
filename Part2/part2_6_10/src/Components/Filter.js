1ff811e71a5f3c2596e30c0a2f1479f5fdfcd542
nkhieu02 <101521246+nkhieu02@users.noreply.github.com>
2022-07-16 14:27:44 +0700

    Move to part2-phonebook

--- Part2/part2_6_10/src/Components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ value, method }) => {
-    return (
-        <div>
-            filter shown with : <input value={value} onChange={method} />
-        </div>
-    );
-}
-
-export default Filter;
--- Part2/part2_6_10/src/Components/Persons.js
+++
@@ -1,8 +0,0 @@
-const Persons = ({ list }) => {
-    return (
-    <div>
-            { list.map(x => (<h4 key={x.id}>{x.name} : {x.number} </h4>))}
-    </div>
-    )
-}
-export default Persons;
